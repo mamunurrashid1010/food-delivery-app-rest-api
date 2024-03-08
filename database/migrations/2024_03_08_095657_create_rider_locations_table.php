c6de7d1cc59c44c59e7e7a47272e671dba13b44c
Muhammed Mamunur Rashid <96053862+mamunurrashid1010@users.noreply.github.com>
2024-03-08 16:13:30 +0600

    controller, model, migration file create for Rider Location

---
+++ database/migrations/2024_03_08_095657_create_rider_locations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rider_locations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('rider_id');
+            $table->foreign('rider_id')->references('id')->on('riders')->onDelete('cascade');
+            $table->double('lat', 10, 6);
+            $table->double('long', 10, 6);
+            $table->timestamp('capture_time');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rider_locations');
+    }
+};
