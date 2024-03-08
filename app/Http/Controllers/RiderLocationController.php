8cadb86d8dea5ba6cf2ad2e6fe3e907e712d2b63
Muhammed Mamunur Rashid <96053862+mamunurrashid1010@users.noreply.github.com>
2024-03-08 16:27:27 +0600

    Functionalities implement for store rider location

--- app/Http/Controllers/RiderLocationController.php
+++ app/Http/Controllers/RiderLocationController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RiderLocation;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RiderLocationController extends Controller
 {
-    //
+    /**
+     * store rider location
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        // data validation
+        $data = $request->validate([
+            'rider_id' => 'required|exists:riders,id',
+            'lat' => 'required|numeric',
+            'long' => 'required|numeric',
+            'capture_time' => 'required|date',
+        ], [
+            'rider_id.required' => 'The rider_id field is required.',
+            'rider_id.exists' => 'The selected rider_id is invalid.',
+            'lat.required' => 'The lat field is required.',
+            'lat.numeric' => 'The lat field must be a number.',
+            'long.required' => 'The long field is required.',
+            'long.numeric' => 'The long field must be a number.',
+            'capture_time.required' => 'The capture_time field is required.',
+            'capture_time.date' => 'The capture_time field must be a valid date.',
+        ]);
+
+        // store data
+        RiderLocation::query()->create($data);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Location stored successfully'
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RiderLocationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+# rider location
+Route::post('store-rider-location',[RiderLocationController::class,'store'])->name('rider.location.store');
